daac7f1eaeca08341b9d8530dc9c4f369b84d389
Grace-Zhong <250467347@qq.com>
2021-10-10 17:31:38 +1030

    Ch4 Ended. TypeScript & Generic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {ProjectListScreen} from "./screens/project-list";
+import {TsReactTest} from "./ass1/try-user-array";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/*<ProjectListScreen />*/}
+      <TsReactTest />
     </div>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,28 @@
 import {useEffect, useState} from "react";
 
-export const isFalsy = (value) => value === 0 ? false : !value;
+export const isFalsy = (value: unknown) => value === 0 ? false : !value;
 
 // 在一个函数里改变传入对象本身是不好的
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
     const result = {...object}
     Object.keys(result).forEach(key => {
+        // @ts-ignore
         const value = result[key]
         if(isFalsy(value)) {
+            // @ts-ignore
             delete result[key]
         }
     })
     return result;
 }
 
-export const useMount = (callBack) => {
+export const useMount = (callBack: () => void) => {
     useEffect(() => {
         callBack()
     }, [])
 }
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = <V>(value: V, delay?: number) => {
     const [debouncedValue, setDebouncedValue] = useState(value);
 
     useEffect(() => {
@@ -43,3 +45,19 @@ export const useDebounce = (value, delay) => {
   //   }, delay);
   // }
 // }
+
+export const useArray = <T>(iniArray: T[]) => {
+    const [value, setValue] = useState(iniArray);
+
+    const add = (newItem: T) => setValue([...value, newItem]);
+
+    const removeIndex = (index: number) => {
+        const copiedArray = [...value];
+        copiedArray.splice(index, 1);
+        setValue(copiedArray);
+    };
+
+    const clear = () => setValue([]);
+
+    return { value, clear, removeIndex, add };
+}
