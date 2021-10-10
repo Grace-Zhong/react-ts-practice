2675df9102c3d2e553dc9c2eb60693a875e59a87
Grace-Zhong <250467347@qq.com>
2021-10-10 19:39:31 +1030

    Ch5.2 Use json-server mock api requests

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {ProjectListScreen} from "./screens/project-list";
 import {TsReactTest} from "./ass1/try-user-array";
+import {LoginScreen} from "./screens/login";
 
 function App() {
   return (
     <div className="App">
       {/*<ProjectListScreen />*/}
-      <TsReactTest />
+      {/*<TsReactTest />*/}
+      <LoginScreen />
     </div>
   );
 }
